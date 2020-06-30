510ff981a6b278e589d1fe913364aec797fee5c1
abelbcarvalho <abelbdecarvalho@gmail.com>
2020-06-30 12:38:08 -0300

    Fadade Class Added

---
+++ src/com/bcarsoft/facade/AFacade.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "../model/GenSQL.cpp"
+
+class AFacade {
+
+    public:
+    
+    std::string generateCreate(GenSQL gen) {
+        return "";
+    }
+    std::string generateInsert(GenSQL gen) {
+        return "";
+    }
+    std::string generateUpdate(GenSQL gen) {
+        return "";
+    }
+    std::string generateSelect(GenSQL gen) {
+        return "";
+    }
+    std::string generateJoin(GenSQL gen) {
+        return "";
+    }
+
+};
---
+++ src/com/bcarsoft/facade/Facade.cpp
@@ -0,0 +1,27 @@
+#include "AFacade.cpp"
+
+class Facade:AFacade {
+
+    public:
+
+    std::string generateCreate(GenSQL gen) {
+        
+    }
+
+    std::string generateInsert(GenSQL gen) {
+        
+    }
+
+    std::string generateUpdate(GenSQL gen) {
+        
+    }
+
+    std::string generateSelect(GenSQL gen) {
+        
+    }
+
+    std::string generateJoin(GenSQL gen) {
+        
+    }
+
+};
